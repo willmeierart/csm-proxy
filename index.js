080f53b97ea71d135fbd568f7927e9151b57d34f
WILL MEIER <willmeierart@gmail.com>
2017-07-28 14:52:43 -0600

    trying out

--- index.js
+++ index.js
@@ -7,7 +7,32 @@ const router = ezc.createRouter()
 router.use(cors())
 
 router.get('/', (req,res)=>{
-  fetchData().then(json=>{res.json(json)})
+  fetchData()
+  .then(json=>{
+    return res.json(json)})
+  .catch(err=>console.log(err))
+})
+router.get('/channels',(req,res)=>{
+  fetchData()
+  .then(json=>{return res.json(json)
+  }).then(res=>{
+    console.log(res)
+    const data = res
+    return data.map((set)=>{
+      return {
+        title:set.title,
+        backgroundimage:set.backgroundimage,
+        backgroundvideo:set.backgroundvideo
+      }
+    })
+
+
+  }).catch(err=>console.log(err))
+})
+router.get('/channels/:id', (req,res)=>{
+  fetchData()
+  .then(json=>{res.json(json)})
+  .then((res)=>{res[req.params.id]})
 })
 
 ezc.startServer(router)
